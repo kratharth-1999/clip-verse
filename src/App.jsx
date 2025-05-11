bb22314192639f16a4456c1aba7b24c6cbe500c0
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-11 18:44:28 +0530

    Added Toastify

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { ToastContainer } from "react-toastify";
+
 function App() {
-    return <h1 className="text-red-500">Clip Verse</h1>;
+    return (
+        <>
+            <h1 className="text-red-500">Clip Verse</h1>
+            <ToastContainer
+                position="top-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+            />
+        </>
+    );
 }
 
 export default App;
