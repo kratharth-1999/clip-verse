4e82cd994bab5e8662f9ba7c552b2d4588aa653b
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-12 22:52:03 +0530

    Added generic get method

---
+++ src/hooks/useGet.jsx
@@ -0,0 +1,58 @@
+import { useState, useCallback } from "react";
+
+const useGet = () => {
+    const [stateOfGetRequest, setStateOfGetRequest] = useState({
+        data: null,
+        gettingData: false,
+        getDataError: null,
+    });
+
+    const getData = useCallback(
+        async (url, headers = {}, options = {}) => {
+            setStateOfGetRequest((prevState) => ({
+                ...prevState,
+                data: null,
+                gettingData: true,
+                getDataError: null,
+            }));
+            try {
+                const responseSync = await fetch(url, {
+                    headers: {
+                        "content-type": "application/json",
+                        ...headers,
+                    },
+                    ...options,
+                });
+                const response = await responseSync.json();
+                if (!responseSync.ok) {
+                    throw new Error(
+                        response.status_message ||
+                            `HTTP error! status: ${responseSync.status}`
+                    );
+                }
+                setStateOfGetRequest((prevState) => ({
+                    ...prevState,
+                    data: response,
+                }));
+            } catch (error) {
+                setStateOfGetRequest((prevState) => ({
+                    ...prevState,
+                    getDataError: error.message || String(error),
+                }));
+            } finally {
+                setStateOfGetRequest((prevState) => ({
+                    ...prevState,
+                    gettingData: false,
+                }));
+            }
+        },
+        [setStateOfGetRequest]
+    );
+
+    return {
+        stateOfGetRequest,
+        getData,
+    };
+};
+
+export default useGet;
