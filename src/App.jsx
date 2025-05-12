76ae969f2243f2842c4210b4da7f5b442116e5f0
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-12 19:04:38 +0530

    Initialized store and created app slice

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { ToastContainer } from "react-toastify";
 import Body from "./components/Body";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
     return (
-        <>
+        <Provider store={store}>
             <Body />
             <ToastContainer
                 position="top-right"
@@ -17,7 +19,7 @@ function App() {
                 pauseOnHover
                 theme="light"
             />
-        </>
+        </Provider>
     );
 }
 
