8114220fb136efdea68dcf71e47f7a88ced2e7fa
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-12 23:00:40 +0530

    Added watch page and routed to it

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import Header from "./Header/Header";
 import Home from "./Home/Home";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import VideoListContainer from "./Home/VideoListContainer";
+import WatchVideo from "./Video/WatchVideo";
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+        children: [
+            {
+                path: "/",
+                element: <VideoListContainer />,
+            },
+            {
+                path: "watch",
+                element: <WatchVideo />,
+            },
+        ],
+    },
+]);
 
 const Body = () => {
     return (
         <>
             <Header />
-            <Home />
+            <RouterProvider router={appRouter} />
         </>
     );
 };
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Sidebar from "./Sidebar";
-import VideoListContainer from "./VideoListContainer";
+import { Outlet } from "react-router";
 
 const Home = () => {
     return (
         <div className="flex px-4 min-h-[calc(100vh_-_81px)]">
             <Sidebar />
-            <VideoListContainer />
+            <Outlet />
         </div>
     );
 };
