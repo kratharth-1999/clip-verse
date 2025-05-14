c75c11eda5363850e704e0c48699c55f74a91b7b
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-14 18:00:31 +0530

    Added video player via iframe

--- src/components/Video/WatchVideo.jsx
+++ src/components/Video/WatchVideo.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { useSearchParams } from "react-router";
 
 const WatchVideo = () => {
-    return <div>WatchVideo</div>;
+    const [searchParams] = useSearchParams();
+    console.log(searchParams.get("v"));
+    return (
+        <div className="flex w-full py-2">
+            <section className="flex flex-col flex-2">
+                <iframe
+                    width="100%"
+                    height="70%"
+                    src={
+                        "https://www.youtube.com/embed/" + searchParams.get("v")
+                    }
+                    title="Youtube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                ></iframe>
+            </section>
+            <section className="flex-1"></section>
+        </div>
+    );
 };
 
 export default WatchVideo;
