ce6415dcdb9e6bdb1b32342103602b3649f9a36e
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-14 19:15:51 +0530

    Cannot show suggestions sections and hence side nav remains empty

--- src/components/Home/VideoListContainer.jsx
+++ src/components/Home/VideoListContainer.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import FilterList from "./FilterList";
 import VideoRowsContainer from "./VideoRowsContainer";
-import { useDispatch } from "react-redux";
-import { openSidebar } from "../../store/slices/appSlice";
 
 const VideoListContainer = () => {
-    const dispatch = useDispatch();
-
-    useEffect(() => {
-        dispatch(openSidebar());
-    }, [dispatch]);
-
     return (
         <div className="flex-1 flex flex-col items-center">
             <FilterList />
