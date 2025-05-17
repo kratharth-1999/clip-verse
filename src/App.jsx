1d0c40b7ce3133db067469783f96a16a7d436c02
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-17 22:38:35 +0530

    Added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,28 @@ import { ToastContainer } from "react-toastify";
 import Body from "./components/Body";
 import { Provider } from "react-redux";
 import store from "./store/store";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
     return (
-        <Provider store={store}>
-            <Body />
-            <ToastContainer
-                position="top-right"
-                autoClose={5000}
-                hideProgressBar={false}
-                newestOnTop={false}
-                closeOnClick={false}
-                rtl={false}
-                pauseOnFocusLoss
-                draggable
-                pauseOnHover
-                theme="light"
-            />
-        </Provider>
+        <>
+            <Provider store={store}>
+                <Body />
+                <ToastContainer
+                    position="top-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
+            </Provider>
+            <Analytics />
+        </>
     );
 }
 
