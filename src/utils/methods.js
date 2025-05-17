42a61ac4824bc2cbe74510f0d09c5397a3ece210
Kratharth <kratharth@Kratharths-MacBook-Air.local>
2025-05-17 13:01:43 +0530

    Added random name and random message generator to generate random live chat messages

--- src/utils/methods.js
+++ src/utils/methods.js
@@ -1,3 +1,5 @@
+import { MESSAGE_LIST, NAME_LIST } from "./constants";
+
 export const debouncedFunction = (fn, delay) => {
     let timeoutId;
     return function (...args) {
@@ -6,4 +8,12 @@ export const debouncedFunction = (fn, delay) => {
             fn(...args);
         }, delay);
     };
+};
+
+export const generateRandomName = () => {
+    return NAME_LIST[Math.floor(NAME_LIST.length * Math.random())];
+};
+
+export const generateRandomMessage = () => {
+    return MESSAGE_LIST[Math.floor(MESSAGE_LIST.length * Math.random())];
 };
